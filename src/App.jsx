30d834df16d7d6f6b1340856b00bd357bd4feeac
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-09-08 15:13:58 +0530

    feat: add userSlice, routing, and page shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Body from "./components/Body";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 const AppRouter = createBrowserRouter([
   {
     path: "",
     Component: Body,
-    children: [{}],
+    children: [
+      {
+        index: true,
+        Component: Dashboard,
+      },
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "profile",
+        Component: Profile,
+      },
+    ],
   },
 ]);
 
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default Body;
