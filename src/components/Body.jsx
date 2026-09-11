a138c17ba4f0e002df1e801755ade708f89fa1ab
Jahnabi Sarma <sarmajahnabi8@gmail.com>
2026-09-11 23:10:24 +0530

    feat: checkingAuth and fetchUser in Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const userData = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      if (userData) return; // already have the user, skip refetch
+
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true, // withCredentials required , Otherwise the cookie won't be sent and you'll get 401s even when logged in.
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      if (error.response?.status === 401) {
+        navigate("/login");
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
